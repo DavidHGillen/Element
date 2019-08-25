fa4b3fd753e3d70ee9594749c4d5228a5a01832f
David <david.h.gillen@gmail.com>
2019-08-25 14:44:04 -0600

    Better store mapping

--- src/renderer/stores/AbstractStore.js
+++ src/renderer/stores/AbstractStore.js
@@ -15,45 +15,47 @@ class AbstractStore {
 
 		this._maxOI = 0;
 		this._lastOI = 0;
-		this._bufferOIList = [];	// contiguous shuffled buffer offset indexes
+		this._lastLookup = 0;
 		this._oiLookupMap = [];		// sparse permanent lookup ids
+		this._bufferLookList = [];	// contiguous shuffled lookup ids
 	}
 
-	initShader(gl) {}
+	initShader(gl) {
+		throw "Abstract funcion";
+	}
 
 	// public
 	////////////////////////////////////////////////////////////////////////////
 	getFreeID() {
 		let freeIndex = -1;
 
-		while(this.oiLookupMap[++freeIndex]) {}
+		while(this._oiLookupMap[++freeIndex]) {}
 
 		if(freeIndex >= this._maxOffset) {
 			throw "Buffer Overflow";
 		}
 
-		this.oiLookupMap[freeIndex] = ++this._lastOI;
-
-		// map points to packed list
-		// list points to offset in buffer
-		// when adding things to the map point them to the end of the list
-		// when removing things from the map collapse the list at target point onwards
-		// when collapsing the list, transpose the last element into the hole
-		// when transposing the last element use the reverse lookup map to find its external id
-		// - pay attention to this reverse id lookup, it solves your issues -
+		this._bufferLookList[this._lastOI] = freeIndex;
+		this._oiLookupMap[freeIndex] = ++this._lastOI;
 
 		return freeIndex;
 	}
 
 	releaseID(id) {
+		// clear the current element
 		let releasedOI = this._oiLookupMap[id];
+		if(releasedOI == 0){ return; }
 		this._oiLookupMap[id] = 0;
-		
 
+		--this._lastOI;
+		if(releasedOI <= this._lastOI) {
+			// move the end into the empty spot
+			let shiftedOI = this._bufferLookList[this._lastOI];
+			this._oiLookupMap[shiftedOI] = releasedOI;
+			this._bufferLookList[releasedOI-1] = shiftedOI;
+		}
 	}
 
 	// private
 	////////////////////////////////////////////////////////////////////////////
-	// resize buffer
-	// compact buffer //// this._lastOI, 
 }
