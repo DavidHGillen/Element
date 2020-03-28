6cdfc3020ef1317d5e835a26966ba62222d1bd33
David <david.h.gillen@gmail.com>
2020-03-27 18:03:10 -0600

    Fixed default cube inversion

--- src/geometry/primitives/CubeHelper.js
+++ src/geometry/primitives/CubeHelper.js
@@ -17,20 +17,22 @@ class CubeHelper {
 
 		valueBlock.select = [0,0,0,0, 0,0,0,0];
 		valueBlock.position = [
-			radius, radius, radius,      -radius, radius, radius,     // front face
-			radius, -radius, radius,     -radius, -radius, radius,
+			radius, -radius, radius,      radius, radius, radius,     // front face
+			radius, -radius, -radius,     radius, radius, -radius,
 
-			radius, radius, -radius,     -radius, radius, -radius,    // back face
-			radius, -radius, -radius,    -radius, -radius, -radius,
+			-radius, -radius, radius,     -radius, radius, radius,    // back face
+			-radius, -radius, -radius,    -radius, radius, -radius,
 		];
 
+
+
 		let tris = new Uint32Array([
-			0,1,3,    0,3,2,    //z+
-			5,4,6,    5,6,7,    //z-
-			1,5,7,    1,7,3,    //x+
-			4,0,2,    4,2,6,    //x-
-			4,5,0,    5,1,0,    //y+
-			3,7,6,    2,3,6,    //y-
+			0,1,3,    0,3,2,
+			5,4,6,    5,6,7,
+			1,5,7,    1,7,3,
+			4,0,2,    4,2,6,
+			4,5,0,    5,1,0,
+			3,7,6,    2,3,6,
 		]);
 
 		let edges = new Uint32Array([
